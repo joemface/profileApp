087b2ae7f029cf456f5f74fbff801c11c3ccf8d6
joemface <joemface@yahoo.com>
2020-07-15 18:03:09 -0400

    working mongodb URI connection

---
+++ config/db.js
@@ -0,0 +1,43 @@
+//here is where we will commit to our mongodb connection
+//first bring in mongoose dependency attributes for db connection
+const mongoose = require('mongoose');
+
+//next we want to bring in our mongoURI config string inside
+//default.json
+const config = require('config');
+
+//using .get for config allows us to get any values already there
+//currently we created mongoURI ourselves. here is where we access it
+const db = config.get('mongoURI');
+
+//now we use mongoose to our advantage for database connectivity
+//first we need something we can call within our server.js app file
+//that's where connectDB comes from, using an asynchronous arrow function.
+//we create a async await function to handle connectivity
+const connectDB = async () => {
+  //when attempting to make any connections
+  //always use a try catch for errors and safety
+  try {
+    //since mongoose returns a promise we want "await" appended in front
+    //so the server awaits a promised response from db variable.
+    //we also throw in a second paramater becuase of the fact that
+    // we get a deprecation warning without it (useNewUrlParser)
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+    });
+    //the next string let's us know in the console that we're connected
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    //log an error if we didn't successfully connect to the db
+    //the err in catch has a '.message' property for logging errors
+    console.error(err.message);
+    //and if we want the application to fail we do that with
+    process.exit(1);
+  }
+};
+
+//lastly we export everything we just achieved (or didn't) with that
+//asynchronous function
+module.exports = connectDB;
+
+//next go back to server.js
--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const express = require('express');
 //app is an express app
 const app = express();
 
+//this next line makes way more sense
+// after completing everything else on this page
+//the very async function we created was exported, right?
+//now that exported data is in this connectDB variable
+// since it's in a different folder we have to say where
+// our connectDB function is ./config/db
+const connectDB = require('./config/db');
+
+///////////////////////////////////////////////////////////////////////////////////
+//now we that we imported our connectDB function we can actually just call it.
+//and you should see your connection confirmation string from the try in db.js
+connectDB();
+
 //this is an endpoint for first test
 //takes a get request to /
 //the second parameter is our callback to our
@@ -18,7 +31,8 @@ const PORT = process.env.PORT || 5000;
 
 //for now we just want a reaction for connecting to PORT
 //and we want the response to be an OK. The second parameter
-// is a callback to confirm our connection.
+// is a callback to confirm our connection which pops up in
+// the terminal.
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 // we could run this app with node server or server.js but
@@ -32,3 +46,16 @@ app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 // now you can type in the terminal npm run server
 // you can now type http://localhost:5000 in the browser
 // or swagger
+
+//for our database we create a folder named config
+//inside that folder we create a file named default.json
+// so going back to one of the dependices we downloaded with npm
+// it's name is config inside the package.json file which is a
+// default json file with defautl json values, in this case,
+// it's for our mongodb string, aka, mongoURI, the one i said
+//to keep for later
+//mongodb string
+// mongodb+srv://<username>:<password>@devconnector-dsgby.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+//now, we don't want to clutter up this file with our db connection
+// logic. So, we create a file in config named db.js
