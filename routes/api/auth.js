1c5a6fb04b7cd9efeb75f131e5840e35705e8347
joemface <joemface@yahoo.com>
2020-07-16 20:04:39 -0400

    working Profile update

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,10 +3,14 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../../models/User');
-
+const bcrypt = require('bcryptjs');
 //bring in middleware for authorization
 const auth = require('../../middleware/auth');
-
+//bring in secret key form config
+const config = require('config');
+//authorization
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
 // @route   GET api/auth
 // @desc    Test route for auth api
 // @access  Public
@@ -23,11 +27,74 @@ router.get('/', auth, async (req, res) => {
   }
 });
 
+//we want to auhorize users for logging in as well.
+//we just copied this function from users.js because
+// logging in works like registration in some aspects
+// @route   POST api/auth
+// @desc    Authenticate user & get token
+// @access  Public
+
+router.post(
+  '/',
+  [
+    //first parameter checks if input is valid email/password
+    //second parameter returns error response if none
+    check('email', 'Email is invalid').isEmail(),
+    check('password', 'Password is requried').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+
+    try {
+      //we want to see if user exists
+      let user = await User.findOne({ email });
+      //if that user email doesn't exist, error
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Email or password is incorrect.' }] });
+      }
+      //we need to make sure the password matches
+      //we can use bcryptjs compare method which returns a promise
+      //pased on comparing entered password and user's email password from db
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Email or password is incorrect.' }] });
+      }
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+
+      console.log(req.body);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 //export the router
 module.exports = router;
 
 //{
-//   "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNWYxMDcyMTkyMGYzZmQ0NjMwYjBkMTkxIn0sImlhdCI6MTU5NDkxMzMwNSwiZXhwIjoxNTk0OTE2OTA1fQ.xNQJYSR9QOmLgc4trkFvT4Ioh018A_cqLbZRT2pYPCE"
+//   "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNWYxMDcyMTkyMGYzZmQ0NjMwYjBkMTkxIn0sImlhdCI6MTU5NDkzMjEzOSwiZXhwIjoxNTk1MjkyMTM5fQ.QTOQRk1Zjq56CJ8GckvUluF036lbHYA64w520HzhYKg"
 //}
 //take the above token to swagger
 //GET http://localhost:5000/api/auth
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,11 +2,132 @@
 //route in a different folder
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+//we use the profile model and user model
+//to shape the payload to our user's profile
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+// @route   GET api/profile/me
+// @desc    Get current user's profile
+// @access  Private
+//we add auth as a second parameter to authorize users.
+//auth came from our own middleware
+//added /me for singular user callback
+router.get('/me', auth, async (req, res) => {
+  try {
+    //always refer to user by their id
+    //since we modeled our Profile user type to be an ObjectId,
+    //that's the id that gets returned.
+    //also, we want the user's name and avatar image
+    //.populate will help us upload that to our user's profile
+    //the info comes from user model, not profile model
+    //the second parameter is the specified info we want from User model
+    const profile = await (
+      await Profile.findOne({ user: req.user.id })
+    ).populate('user', ['name', 'avatar']);
 
-// @route   GET api/profile
-// @desc    Test route for profile api
-// @access  Public
-router.get('/', (req, res) => res.send('Profile route'));
+    //if no profile
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
 
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   Post api/profile
+// @desc    Create or update a user profile
+// @access  Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'Status is required').not().isEmpty(),
+      check('skills', 'Skills are required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    //get everything from the body of the page
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body;
+
+    //build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      //.split says we want to split this string into an array
+      //and we do that by using .split signifying the next index
+      // with a separator.
+      // Our separator is a comma.
+      //in order to ignore white space we map through the array
+      //for each skill and trim it.
+      profileFields.skills = skills.split(',').map((skill) => skill.trim());
+    }
+
+    // Build social object for youtube and the like
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+
+      //if profile exists in db
+      if (profile) {
+        //Update
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        return res.json(profile);
+      } //end of update
+
+      //create if not found
+      profile = new Profile(profileFields);
+
+      await profile.save(); //save profile
+      res.json(profile); //send back profile json
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 //export the router
 module.exports = router;
+
+//now go to swagger and test http://localhost:5000/api/profile/me
+//copy and past token next to the header x-auth-token
+// token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNWYxMDcyMTkyMGYzZmQ0NjMwYjBkMTkxIn0sImlhdCI6MTU5NDkzMjEzOSwiZXhwIjoxNTk1MjkyMTM5fQ.QTOQRk1Zjq56CJ8GckvUluF036lbHYA64w520HzhYKg
