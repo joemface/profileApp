c5dc6ce281e984997421a909cdeea21b234c7171
joemface <joemface@yahoo.com>
2020-07-19 17:15:53 -0400

    finished with profile.js for now

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,3 +1,5 @@
+//make sure to npm run server in terminal before
+//making api requests in swagger
 //we need to bring an express router for having this
 //route in a different folder
 const express = require('express');
@@ -371,11 +373,31 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 // @access  Public
 router.get('/github/:username', async (req, res) => {
   try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        'githubClientId'
+      )}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: 'No Github profile found' });
+      }
+
+      res.json(JSON.parse(body));
+    });
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error. GET api/github/:username');
   }
 });
+
 //export the router
 module.exports = router;
 
